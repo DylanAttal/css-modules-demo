ec1b9edc03a93efa167c516f9d006bae51bad007
Dylan Attal <attal.dylan@gmail.com>
2020-07-31 16:32:10 -0400

    makes header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='title'>My Library Demo App</h1>
+      <h3 className='author'>Author: Dylan Attal</h3>
+    </div>
+  )
+}
